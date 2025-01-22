f6fb08355f4cb3be72c2263788ec5a4e496e5457
vxnsin <luiskanzewitsch@gmail.com>
2025-01-22 19:45:44 +0100

    Refactor WebSocket client to use secure connection and streamline message handling

--- websocket_client.js
+++ websocket_client.js
@@ -1,35 +1,34 @@
 const WebSocket = require('ws');
 
 
-const wstest = new WebSocket('ws://localhost:3012');
 const ws = new WebSocket('wss://neopin.storagevault.me/ws');
 
-wstest.on('open', () => {
+ws.on('open', () => {
   console.log('Connected to the server');
 
   const authData = { deviceId: 'device-1', password: 'neopin123' };
   console.log('Sending auth data:', authData);
 
-  wstest.send(JSON.stringify(authData));
+  ws.send(JSON.stringify(authData));
 
   setTimeout(() => {
-    wstest.send(JSON.stringify({ ping: 'Hello from client 1' }));
+    ws.send(JSON.stringify({ ping: 'Hello from client 1' }));
   }, 1000); 
 });
 
-wstest.on('message', (data) => {
+ws.on('message', (data) => {
     const parsedMessage = JSON.parse(data);
   console.log('Received from server:', parsedMessage);
 
     if(parsedMessage.ping) {
-      wstest.send(JSON.stringify({latitude: "50.1234", longitude: "-501.123"}))
+      ws.send(JSON.stringify({latitude: "50.1234", longitude: "-501.123"}))
     }
 });
 
-wstest.on('error', (error) => {
+ws.on('error', (error) => {
   console.error('WebSocket error:', error);
 });
 
-wstest.on('close', () => {
+ws.on('close', () => {
   console.log('Connection closed');
 });
